7dc8735cb717244882ac449b70a5975f0ca9a772
shivapabbu1 <93253969+shivapabbu1@users.noreply.github.com>
2023-10-25 11:39:41 +0530

    Update index.js

--- Javascript/day-oct-25/index.js
+++ Javascript/day-oct-25/index.js
@@ -12,3 +12,21 @@ a=b;
 b=t;
 console.log("Afer swaping of a and b");
 console.log("a="+a +"and"+"b="+b);
+
+//Find max and min
+
+let x=500, y=600,z=700;
+let max,min;
+
+if(x>y){
+max=x;
+min=y;
+}
+else{
+    max=y;
+    min=x
+}
+if(z>max) max=z;
+if(z<min) min=z;
+console.log("max="+max);
+console.log("min="+min);
