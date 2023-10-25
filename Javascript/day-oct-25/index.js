a2d559a13f6292d95a26be150c195b85e7cab043
shivapabbu1 <93253969+shivapabbu1@users.noreply.github.com>
2023-10-25 11:27:22 +0530

    Add files via upload

---
+++ Javascript/day-oct-25/index.js
@@ -0,0 +1,14 @@
+console.log("Welcome to  Unocarrer")
+
+// document.body.innerHTML=" Hello Good Morning javascript"
+
+let a=300;
+let b=400;
+console.log("Before swaping of a and b");
+console.log("a="+a +"and"+"b="+b);
+let t;
+t=a;
+a=b;
+b=t;
+console.log("Afer swaping of a and b");
+console.log("a="+a +"and"+"b="+b);
